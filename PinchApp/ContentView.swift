6f8705ad8d4630ffc53389b541f24ba3cd9b1a4a
Leonardo Soares <leonardofs88@live.com>
2024-05-04 15:28:35 +0100

    Initial Commit

---
+++ PinchApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PinchApp
+//
+//  Created by Leonardo Soares on 04/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Leonardo Soares on 04/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
